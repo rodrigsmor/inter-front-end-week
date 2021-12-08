7cad457541f4af0f683e17e9128342b822dbd6a8
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2021-12-08 16:32:31 -0300

    Create controller/service and connect to database

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import { createConnection } from 'typeorm';
+import { globalErrors } from './middlewares/globalErrors';
+import routes from './routes';
 
-const app = express();
-const PORT = 3333;
-
-app.get('/', (request, response) => response.send('Express + Typescript Server'));
-
-app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+createConnection().then(connection => {
+    const app = express();
+    const PORT = 3333;
+    
+    app.use(routes);
+    app.use(globalErrors);
+    
+    app.listen(PORT, () => {
+        console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
+    });
+}).catch((error) => {
+    console.log('Unable to connect to the database', error)
 });
---
+++ back-end/src/resources/user/user.service.ts
@@ -0,0 +1,27 @@
+import { getRepository } from 'typeorm';
+import { User } from '../../entity/Users';
+import md5 from 'crypto-js/md5';
+import { UserSignUp } from './dtos/user.signup.dtos';
+import { UserSignIn } from './dtos/user.singin.dtos'
+import AppError from '../../shared/error/AppError';
+
+export default class UserService {
+    async signin(user: UserSignIn) {
+        const userRepository = getRepository(User);
+
+        const { email, password } = user;
+        const passwordHash = md5(password).toString();
+
+        const existUser = await userRepository.findOne({ where: { email, password: passwordHash }});
+
+        if(!existUser) {
+            throw new AppError('Usuário não encontrado', 401);
+        }
+
+        return existUser;
+    }
+
+    async signup(user: UserSignUp) {
+        return null;
+    }
+}

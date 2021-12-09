9a81c1d52cef3df0b7f66cbaf1986c6d3deb1d61
Rodrigo Moreira <rodrigomoreiradasilva.of@gmail.com>
2021-12-09 09:13:06 -0300

    Develop all back-end system

--- back-end/src/resources/user/user.service.ts
+++ back-end/src/resources/user/user.service.ts
@@ -73,4 +73,18 @@ export default class UserService {
 
         return { accessToken: token };
     }
+
+    async me(user: Partial<User>) {
+        const userRepository = getRepository(User);
+        const currentUser = await userRepository.findOne({ where: { id: user.id }});
+
+        if(!currentUser) {
+            throw new AppError('Usuário não encontrado', 401);
+        }
+
+        // @ts-expect-error ignore
+        delete currentUser.password
+
+        return currentUser;
+    }
 }
